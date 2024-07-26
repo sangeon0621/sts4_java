9039b3dc632859780c2010767d8a570520e8d026
sangeon0621 <tkddjs9604@gmail.com>
2024-07-26 10:36:35 +0900

    java

---
+++ src/ch02/LongEx.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class LongEx {
+
+	public static void main(String[] args) {
+
+		
+		long var = 1000000000;
+		
+		System.out.println(var);
+		
+		
+		
+	}
+
+}
---
+++ src/ch02/VariableEx.java
@@ -0,0 +1,25 @@
+package ch02;
+
+public class VariableEx {
+	
+//	상수 표기법
+	static final int SOCIAL_SECURITY_NUMBER = 11111;
+	
+//	스네이크 표기법
+	int social_security_number;
+	
+//	파스칼 표기법
+	int SocialSecurityNunber;
+	
+//	카멜 표기법
+	int socialSecurityNunber;
+	
+	
+	public static void main(String[] args) {
+
+
+		
+		
+	}
+
+}
