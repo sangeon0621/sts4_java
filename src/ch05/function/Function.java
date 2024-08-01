38c68f261675d2db7081b89c0101f1bccea60f21
sangeon0621 <tkddjs9604@gmail.com>
2024-08-01 12:42:35 +0900

    java

---
+++ src/ch05/function/Function.java
@@ -0,0 +1,17 @@
+package ch05.function;
+
+public class Function {
+
+	public static void main(String[] args) {
+
+
+		int result = sum(1,3);
+		System.out.println(result);
+		
+	}
+
+	public static  int sum(int a, int b) {
+		return a + b;
+	}
+	
+}
